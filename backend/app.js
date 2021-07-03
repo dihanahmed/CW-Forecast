f659ca684aafa0a11c529eab84afcc8881dca9c8
Jisan <jishananam@iut-dhaka.edu>
2021-07-04 03:46:29 +0600

    Can post data to postman :) silly mistake took me about one hour to find out

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const loginRouter = require('./route/login.controller')
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.get("/express",((req, res) => {
     console.log("Sending data");
     res.send('hello world');
+
 /*
     This sends data to react
     res.json({helloWorld: "hello world"});
 */
 }))
 app.use('/',loginRouter);
+app.post('/post',(req, res) =>{
+    const name = req.body.name;
+    console.log("please do ")
+    console.log(name)
+    res.send(name)
+})
 
 app.listen(8001)
 module.exports=app;
--- backend/route/login.controller.js
+++ backend/route/login.controller.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const route = express.Router()
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
 route.get('/', (req, res) => {
     console.log("Done my boi")
-    res.send("done");
+    res.send("done")
     res.end()
+})
+route.post('/',(req,res) => {
+    const name = req.body.name
+    res.send(name)
 })
 
 module.exports = route
