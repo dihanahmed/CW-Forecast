0191db558a4672b29f9798cac13fa54576bf2bf4
Jisan <jishananam@iut-dhaka.edu>
2021-07-04 03:02:02 +0600

    Can submit  get request

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
-
+const loginRouter = require('./route/login.controller')
 const app = express();
 
 app.get("/express",((req, res) => {
     console.log("Sending data");
+    res.send('hello world');
+/*
+    This sends data to react
     res.json({helloWorld: "hello world"});
+*/
 }))
+app.use('/',loginRouter);
 
 app.listen(8001)
+module.exports=app;
