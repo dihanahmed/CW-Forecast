67b1ec6c69b2a90161aaaaaeb7a62f36e70104a5
Jisan <jishananam@iut-dhaka.edu>
2021-07-05 15:24:31 +0600

    Client can insert data into database and the password is also encrypted

--- backend/route/api/authenticate/registration.route.js
+++ backend/route/api/authenticate/registration.route.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const route = express.Router()
 const checkRegistration=require('../../../middleware/api/authentication/registration.middleware')
-const confirmRegistration=require('../../../controller/api/authenticate/registration.controller')
+const {confirmRegistration,sendData}=require('../../../controller/api/authenticate/registration.controller')
 
 route.get('/',(req, res)=>{
     res.send('Registration Successfully completed')
 })
-route.post('/',checkRegistration,confirmRegistration)
+route.post('/',checkRegistration,sendData)
 
 module.exports=route
