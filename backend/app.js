24aa7e41226af6501793a89ad064eec8123dae23
Jisan <jishananam@iut-dhaka.edu>
2021-07-06 23:00:26 +0600

    Can Fetch data from API

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,24 @@ const app = express();
 const addAuthenticateOption = require("./api/blog-api/middlewares/authentication/checkAuthentication");
 const payment=require("./api/payment-api/routes/payment.controller")
 const SSLCommerzPayment = require('sslcommerz')
+var ForecastIo = require('forecastio');
 
+
+var forecastIo = new ForecastIo('2286dd2204e405b21ad2dec95e789d49');
+
+app.get('/getWeather',(req, res) =>{
+    forecastIo.forecast('51.506', '-0.127').then(function(data) {
+
+        console.log(JSON.stringify(data, null, 2));
+
+/*
+        console.log(JSON.stringify(data.currently.icon,null,2))
+*/
+        res.end()
+    });
+    var forecastIoEnd = new ForecastIo('2286dd2204e405b21ad2dec95e789d49', {timeout: 30*1000});
+
+})
 configureApp = ()=>{
     app.use(express.json());
     app.use(express.urlencoded({extended:false}))
@@ -14,47 +31,9 @@ configureApp = ()=>{
 
 }
 
+
 configureApp();
 
-/*app.get('/init', (req, res) => {
-    const data = {
-        total_amount: 100,
-        currency: 'BDT',
-        tran_id: 'REF123', // use unique tran_id for each api call
-        success_url: 'http://localhost:3030/success',
-        fail_url: 'http://localhost:3030/fail',
-        cancel_url: 'http://localhost:3030/cancel',
-        ipn_url: 'http://localhost:3030/ipn',
-        shipping_method: 'Courier',
-        product_name: 'Computer.',
-        product_category: 'Electronic',
-        product_profile: 'general',
-        cus_name: 'Customer Name',
-        cus_email: 'customer@example.com',
-        cus_add1: 'Dhaka',
-        cus_add2: 'Dhaka',
-        cus_city: 'Dhaka',
-        cus_state: 'Dhaka',
-        cus_postcode: '1000',
-        cus_country: 'Bangladesh',
-        cus_phone: '01711111111',
-        cus_fax: '01711111111',
-        ship_name: 'Customer Name',
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
-        ship_postcode: 1000,
-        ship_country: 'Bangladesh',
-    };
-    const sslcommer = new SSLCommerzPayment('no60e394d994c14', 'no60e394d994c14@ssl',false) //true for live, false for sandbox
-    sslcommer.init(data).then(apiResponse => {
-        // Redirect the user to payment gateway
-        let GatewayPageURL = apiResponse.GatewayPageURL
-        res.redirect(GatewayPageURL)
-        console.log('Redirecting to: ', GatewayPageURL)
-    });
-})*/
 
 
 
