772aa65a4714275410606b9568767feae0398648
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-06 23:21:17 +0600

    Refactor: make login.controller more readable

--- backend/api/blog-api/controllers/AuthenticationController/login.controller.js
+++ backend/api/blog-api/controllers/AuthenticationController/login.controller.js
@@ -5,25 +5,31 @@ const User = require("../../models/users");
 
 'use strict';
 
-const canLogin = (req, res) => {
+function acceptOrRejectLogin(email, res) {
+    return function (err, isValid) {
+        if (isValid) {
+            sessionStorage.setItem('user', email)
+            res.json({loginStatus: true});
+        } else {
+            res.json({loginStatus: false});
+        }
+
+    };
+}
+
+
+
+const tryLogin = (req, res) => {
 
     const email = req.body.email;
     const password = req.body.password;
     User.findOne({email})
         .then((user)=>{
             if(!user)  return res.json({loginStatus: false});
-
-            bcrypt.compare(password, user.password, function (err, isValid) {
-                if (isValid) {
-                    sessionStorage.setItem('user', email)
-                    res.json({loginStatus:true});
-                } else {
-                    res.json({loginStatus:false});
-                }
-
-            })
+            bcrypt.compare(password, user.password, acceptOrRejectLogin(email, res))
         })
-
 }
 
-module.exports = canLogin
+
+
+module.exports = tryLogin
