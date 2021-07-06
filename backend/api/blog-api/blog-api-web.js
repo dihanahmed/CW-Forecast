b1266860b715627395e719d039b93f443b783e78
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-06 15:17:31 +0600

    Fix is User Authenticated? From Request Previously it always returned true

--- backend/api/blog-api/blog-api-web.js
+++ backend/api/blog-api/blog-api-web.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const loginRouter = require('./routes/authenticate/login.route')
 let registerRouter=require('./routes/authenticate/registration.route')
-
+const addAuthenticateOption = require("./middlewares/authentication/checkAuthentication");
 
 const route = express.Router()
 const app = express();
 
-
+route.use(addAuthenticateOption.auth)
 route.use('/login',loginRouter);
 route.use('/register',registerRouter);
 
--- backend/api/blog-api/middlewares/authentication/checkAuthentication.js
+++ backend/api/blog-api/middlewares/authentication/checkAuthentication.js
@@ -4,7 +4,8 @@ const sessionStorage = require('sessionstorage');
 module.exports ={
    auth : (request, response, next)=>{
        let user = sessionStorage.getItem('user');
-       request.body.authenticated = user !== undefined;
+       console.log(user);
+       request.body.authenticated = user !== null;
        next();
    }
 
