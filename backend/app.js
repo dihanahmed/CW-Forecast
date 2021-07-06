c70b48470ff08379576593b0e71de038eab45569
Jisan <jishananam@iut-dhaka.edu>
2021-07-06 23:12:54 +0600

    Can Exclude data and get proper data from the call

--- backend/app.js
+++ backend/app.js
@@ -7,20 +7,24 @@ const payment=require("./api/payment-api/routes/payment.controller")
 const SSLCommerzPayment = require('sslcommerz')
 var ForecastIo = require('forecastio');
 
-
+var options = {
+    units:'si',
+    exclude:'minutely,hourly,daily,hourly'
+}
 var forecastIo = new ForecastIo('2286dd2204e405b21ad2dec95e789d49');
 
 app.get('/getWeather',(req, res) =>{
-    forecastIo.forecast('51.506', '-0.127').then(function(data) {
+    forecastIo.forecast('51.506', '-0.127',options).then(function(data) {
 
         console.log(JSON.stringify(data, null, 2));
+        res.send(JSON.stringify(data,null,2))
 
 /*
         console.log(JSON.stringify(data.currently.icon,null,2))
 */
+        var forecastIoEnd = new ForecastIo('2286dd2204e405b21ad2dec95e789d49', {timeout: 30*1000});
         res.end()
     });
-    var forecastIoEnd = new ForecastIo('2286dd2204e405b21ad2dec95e789d49', {timeout: 30*1000});
 
 })
 configureApp = ()=>{
