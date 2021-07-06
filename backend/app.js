325c652c89b69e3cd371672a76005ece2c3cda8a
Jisan <jishananam@iut-dhaka.edu>
2021-07-07 01:15:48 +0600

    solve merge conflict and removed package.json-locked

--- backend/app.js
+++ backend/app.js
@@ -7,26 +7,7 @@ const payment=require("./api/payment-api/routes/payment.controller")
 const getWeather=require("./api/weather-api/routes/weather.route")
 const SSLCommerzPayment = require('sslcommerz')
 
-/*var options = {
-    units:'si',
-    exclude:'minutely,hourly,daily,hourly'
-}
-var forecastIo = new ForecastIo('2286dd2204e405b21ad2dec95e789d49');
 
-app.get('/getWeather',(req, res) =>{
-    forecastIo.forecast('51.506', '-0.127',options).then(function(data) {
-
-      /!*  console.log(JSON.stringify(data, null, 2));
-        res.send(JSON.stringify(data,null,2))
-*!/
-
-        res.send(JSON.stringify(data.currently.icon,null,2))
-
-        var forecastIoEnd = new ForecastIo('2286dd2204e405b21ad2dec95e789d49', {timeout: 30*1000});
-        res.end()
-    });
-
-})*/
 configureApp = ()=>{
     app.use(express.json());
     app.use(express.urlencoded({extended:false}))
