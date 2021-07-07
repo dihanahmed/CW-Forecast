97a1b32603235787fc167b3ad62a708392fc3299
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-07 21:04:12 +0600

    add blog route to web

--- backend/api/blog-api/routes/blog/blog.route.js
+++ backend/api/blog-api/routes/blog/blog.route.js
@@ -2,14 +2,13 @@ const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
 
-router.get('/new', (req, res) => {
-    res.render('articles/new', { article: new Article() })
-})
-
-router.get('/edit/:id', async (req, res) => {
+async function edit (req, res)  {
     const article = await Article.findById(req.params.id)
     res.render('articles/edit', { article: article })
-})
+}
+
+
+router.get('/edit/:id', edit )
 
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug })
