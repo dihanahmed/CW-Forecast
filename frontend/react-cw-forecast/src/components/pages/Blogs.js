20ed6c719b1dad45fbaf599cbd3386476c3976c6
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-09 02:14:34 +0600

    Update Blogs

--- frontend/react-cw-forecast/src/components/pages/Blogs.js
+++ frontend/react-cw-forecast/src/components/pages/Blogs.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Blogs() {
-  return <h1 className='services'>SERVICES</h1>;
+  return <h1 className='services'>Blogs</h1>;
 }
