5437457de26f439b16623860f1e646c738b7be23
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-08 23:35:44 +0600

    make Navbar And Button

--- frontend/react-cw-forecast/src/App.js
+++ frontend/react-cw-forecast/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Navbar from './components/Nabvbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path = "/" exact/>
+        </Switch>
+
+        </Router>
+
+    </>
+
+
   );
 }
 
--- frontend/react-cw-forecast/src/components/Nabvbar.js
+++ frontend/react-cw-forecast/src/components/Nabvbar.js
@@ -1,11 +1,51 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 
-function Nabvbar() {
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        CW <i className="fab fa-typo3"/>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? "nav-menu-active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/weather" className="nav-links" onClick={closeMobileMenu}>
+                                WeatherAPI
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/climate-simulator" className="nav-links" onClick={closeMobileMenu}>
+                                Climate Simulator
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/blog" className="nav-links" onClick={closeMobileMenu}>
+                                Blogs
+                            </Link>
+                        </li>
 
+                        <li className="nav-item">
+                            <Link to="/donate" className="nav-links" onClick={closeMobileMenu}>
+                                Donate
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
 
             </nav>
@@ -13,3 +53,5 @@ function Nabvbar() {
         </>
     )
 }
+
+export default Navbar;
