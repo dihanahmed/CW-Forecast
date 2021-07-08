cfd79048fd3386c39e8c865cf6ca12f6847f30ea
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-09 01:45:40 +0600

    Add landing description

--- frontend/react-cw-forecast/src/components/HeroSection.js
+++ frontend/react-cw-forecast/src/components/HeroSection.js
@@ -7,25 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
-      </div>
+      <h1>CW-Forecast</h1>
+      <p>A website for research on weather and climate for scholars with environment Concerns</p>
     </div>
   );
 }
