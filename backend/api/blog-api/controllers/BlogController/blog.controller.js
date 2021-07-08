8b63868dffa57e8a6e62ed40e5033b754433ca34
hasnaine ahmed dihan <dihanahmed205@gmail.com>
2021-07-09 02:06:35 +0600

    exports has been changed

--- backend/api/blog-api/controllers/BlogController/blog.controller.js
+++ backend/api/blog-api/controllers/BlogController/blog.controller.js
@@ -50,4 +50,8 @@ function blogAll(req,res){
 }
 
 
-module.exports=routers
+module.exports={
+    makeBlog,
+    blogbyEmail,
+    blogAll
+}
--- backend/api/donation-api/Controllers/DonationController/DonatonController.js
+++ backend/api/donation-api/Controllers/DonationController/DonatonController.js
@@ -1,6 +1,46 @@
 const Donation = require("../../Models/Donations");
 
 
+
+
+
+function fetchDonations(req,res){
+    
+    const {donorname, email, phone, treeID }=req.body
+
+    //check required field
+    if(!donorname || !email || !phone ||!treeID){
+        console.log("please fill up the form to complete donation form")
+    }
+    else{
+        const newDonation= new Donation({
+           donorname,
+           email,
+           phone,
+           treeID
+        })
+        newDonation.save()
+    
+        
+    }
+}
+
+function showDonate(req,res){
+    Donation.findAll()
+    .then(donate=>{
+        if(donate){
+            res.json(Donation)
+
+        }
+    })
+}
+
+
+module.exports={
+    fetchDonations,
+    showDonate
+}
+
 module.exports ={
     donate: (request,response)=>{
         const donation = new Donation(request.body.data);
