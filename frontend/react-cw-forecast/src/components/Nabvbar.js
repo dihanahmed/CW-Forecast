0e94a2ca54007b7cfb0d52e324a7a0893b77113b
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-09 00:11:30 +0600

    Add App.css configs and some navbar button settings

--- frontend/react-cw-forecast/src/components/Nabvbar.js
+++ frontend/react-cw-forecast/src/components/Nabvbar.js
@@ -1,11 +1,24 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import {Button} from "./Button";
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button,setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton= () =>{
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize',showButton)
 
 
     return (
@@ -45,7 +58,17 @@ function Navbar() {
                                 Donate
                             </Link>
                         </li>
+                        <li className="nav-item">
+                            <Link to="/sign-up"
+                                  className="nav-links"
+                                  onClick={closeMobileMenu}
+                            >
+                                Sign Up
+                            </Link>
+                        </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+
                 </div>
 
             </nav>
