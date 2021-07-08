c730ee3e1d19ca830f9099e63ec6ed0541dcc67e
Sadman Saadat <sadmansaadat99@gmail.com>
2021-07-08 13:14:52 +0600

    proxy setup and blogpost page structure

--- frontend/react-main/src/pages/login.js
+++ frontend/react-main/src/pages/login.js
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-	return (
-		<div>
-				<h1>login</h1>
-				
-		</div>
-	)
-};
 
-export {Login};
+class Login extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.changeEmail = this.changeEmail.bind(this)
+        this.changePassword = this.changePassword.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+    }
+
+    changeEmail(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    changePassword(event){
+        this.setState({
+            password:event.target.value
+        })
+    }
+
+	onSubmit(event){
+        event.preventDefault()
+
+        const registered = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        axios.post('http://localhost:8001/api/blog-api/register', registered)
+        .then((response) => {
+          this.setState({ redirect: true })
+        });
+
+        
+
+        this.setState({
+            email:'',
+            password:''
+        })
+		this.props.history.push('/dashboard');
+    }
+
+    
+
+    render(){
+        return(
+            <div>
+                <div classname= 'container'>
+                    <div classname = 'form-div'>
+                        <form onSubmit={this.onSubmit}>
+
+                            <input type = 'text' placeholder='email' onChange = {this.changeEmail} value = {this.state.email} classname = 'form-control from-group'/>
+
+                            <input type = 'password' placeholder='password' onChange = {this.changePassword} value = {this.state.password} classname = 'form-control from-group'/>
+
+                            <input type = 'submit' classname='btn btn-danger btn-block' value = 'Submit' />
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
+//export {App};
