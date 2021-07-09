922730ebc367f3450ef6fc6f985497ab26c95dce
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-09 20:29:07 +0600

    Add Registratin And Login Button In Blog

--- frontend/react-main/src/pages/blog.js
+++ frontend/react-main/src/pages/blog.js
@@ -1,31 +1,54 @@
 import React from "react";
+import {AppBar, Toolbar} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import {getUseStyles} from "./DonationStyles";
 
 class Blogpage extends React.Component {
+
+
+
     render() {
+        const useStyles = getUseStyles();
+        const classes = useStyles;
         const { history } = this.props
 
         return (
             <div>
-                <div>
-                    <button
-                        onClick={() => {
-                            history.push("/registration")
-                        }}
-                    >
-                        Register Your Account
-                    </button>
 
-                </div>
+                <AppBar className = {classes.component}>
+                    <Toolbar className = {classes.container}>
+                        <Typography variant="h4"> Blog</Typography>
 
-                <div>
-                    <button
-                        onClick={() => {
-                           history.push("/login")
-                        }}
-                    >
-                        Login to Your Account
-                    </button>
-                </div>
+                        <Typography variant="h4">
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        history.push("/registration")
+                                    }}
+                                >
+                                    Register Your Account
+                                </button>
+
+                            </div>
+
+                        </Typography>
+
+                        <Typography variant="h4">
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        history.push("/login")
+                                    }}
+                                >
+                                    Login to Your Account
+                                </button>
+                            </div>
+
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+
             </div>
         )
     }
