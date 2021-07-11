7f70cb949cb57fd188d1c2fa374f0d37371abcf7
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-11 20:42:57 +0600

    Updated cities

--- backend/api/weather-api/WeatherController/cities.js
+++ backend/api/weather-api/WeatherController/cities.js
@@ -1,32 +1,23 @@
 const cities ={
     DHAKA: {
-        latitude: 0,
-        longitude: 0
+        latitude: 23.810331,
+        longitude: 90.412521
     },
     CHITTAGONG: {
         latitude: 22.341900,
         longitude: 91.815536
     },
-    RAJSHAHI: {
-        latitude: 0,
-        longitude: 0
-    },
+
     KHULNA: {
         latitude: 22.820000,
         longitude: 89.550003
     },
-    BARISHAL: {
-        latitude: 0,
-        longitude: 0
-    },
+
     SYLHET: {
         latitude: 24.894802,
         longitude: 91.869034
     },
-    MYMENSINGH: {
-        latitude: 0,
-        longitude: 0
-    },
+
     JASHORE: {
         latitude: 23.170664,
         longitude: 89.212418
