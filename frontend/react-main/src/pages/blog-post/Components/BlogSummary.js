35c20be656d5f20a1470652aed4ad65db5c80b30
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-13 22:06:39 +0600

    Blog list

--- frontend/react-main/src/pages/blog-post/Components/BlogSummary.js
+++ frontend/react-main/src/pages/blog-post/Components/BlogSummary.js
@@ -1,14 +1,36 @@
 import React from "react";
 import * as PropTypes from "prop-types";
+import "./BlogSummary.scss";
 
 export class BlogSummary extends React.Component {
     render() {
         console.log(this.props.blog._id);
         return <>
-            <h4> {this.props.blog.title}</h4>
-            {
+            <div className="container mt-5">
 
-            }
+
+                <div className="blog-card__info">
+                    <h5>{this.props.blog.title}</h5>
+                    <p>
+                        <a href="#" className="icon-link mr-3">
+                            <i className="fa fa-pencil-square-o">
+
+                            </i>{this.props.blog.email}</a>
+                        <a href="#" className="icon-link">
+                            <i className="fa fa-comments-o">
+
+                            </i> 150</a>
+                    </p>
+                    <p>{
+                        this.props.blog.content.substring(0,64)
+                    }...</p>
+                    <a href="#" className="btn btn--with-icon">
+                        <i className="btn-icon fa fa-long-arrow-right">
+
+                        </i>READ MORE</a>
+                </div>
+            </div>
+
 
         </>;
     }
