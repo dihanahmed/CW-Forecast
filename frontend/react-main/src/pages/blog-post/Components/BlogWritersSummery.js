905eba548b2520569ad540bcd61036b264fff7e3
Labnan <khalidmasum@iut-dhaka.edu>
2021-10-09 17:18:02 +0600

    fix remove, add edit button

--- frontend/react-main/src/pages/blog-post/Components/BlogWritersSummery.js
+++ frontend/react-main/src/pages/blog-post/Components/BlogWritersSummery.js
@@ -8,11 +8,14 @@ import {Card, Container, Row} from "react-bootstrap";
 export class BlogWriterSummery extends React.Component {
 
     removeWithID(id) {
-        const sendData = {blog_id: id};
-        axios.post("http://localhost:8001/blog-api/blog/post-remove", sendData);
-        window.location.reload();
+        if (window.confirm("Are You Sure?")) {
 
+            const sendData = {blog_id: id};
+            axios.post("http://localhost:8001/blog-api/blog/post-remove", sendData).then(data => {
+                window.location.reload();
+            })
 
+        }
 
     }
 
@@ -27,16 +30,30 @@ export class BlogWriterSummery extends React.Component {
                         <Row className="m-auto align-self-center">
                             <Card style={{width: 1000}} border="success" className="rounded-3">
                                 <ReaderGuide blog={this.props.blog} history={this.props.history}/>
+                                <div className="justify-content-center"
+                                     style={{display: 'flex', flexDirection: 'row'}}
+                                >
+                                    <div
+                                        className="d-flex flex-row justify-content-end ">
+                                        <div
+                                            className="btn img-fluid btn-outline-success rounded-pill btn-lg img_container btn-sm">
+                                            <img alt="Edit" src="/image/edit.png"/>
+                                        </div>
+                                    </div>
 
-                                <div
-                                    onClick={() => {
-                                        this.removeWithID(this.props.blog._id)
-                                    }}
-                                    className="d-flex flex-row justify-content-end ">
-                                    <div className="btn img-fluid btn-outline-danger rounded-pill btn-lg img_container btn-sm">
-                                        <img alt="Remove" src="https://cdn.iconscout.com/icon/free/png-32/bin-61-267454.png"/>
+                                    <div
+                                        onClick={() => {
+                                            this.removeWithID(this.props.blog._id)
+                                        }}
+                                        className="d-flex flex-row justify-content-end ">
+                                        <div
+                                            className="btn img-fluid btn-outline-danger rounded-pill btn-lg img_container btn-sm">
+                                            <img alt="Remove"
+                                                 src="https://cdn.iconscout.com/icon/free/png-32/bin-61-267454.png"/>
+                                        </div>
                                     </div>
                                 </div>
+
 
                             </Card>
                         </Row>
