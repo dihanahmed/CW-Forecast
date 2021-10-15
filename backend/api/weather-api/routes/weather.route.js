777ba7e181d146766a7eb97f06819ec6d2348219
Jisan <jishananam@iut-dhaka.edu>
2021-10-15 09:44:25 +0600

    Implemented new feature to get hourly data

--- backend/api/weather-api/routes/weather.route.js
+++ backend/api/weather-api/routes/weather.route.js
@@ -11,7 +11,8 @@ var options = {
 var forecastIo = new ForecastIo('2286dd2204e405b21ad2dec95e789d49');
 
 route.post('/',weatherController.fetchWeatherByCity);
-route.get('/',weatherController.getLocationByGeolocation)
+route.get('/',weatherController.getLocationByGeolocation);
+route.post('/hourly',weatherController.fetchWeatherByCityAndHourly)
 
 
 //
