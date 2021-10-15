00fea4c35e5b9da31fdca4b10fcbabf7022910b2
Jisan <jishananam@iut-dhaka.edu>
2021-10-15 12:33:43 +0600

    Changes in donations

--- frontend/react-main/src/pages/donation.js
+++ frontend/react-main/src/pages/donation.js
@@ -45,47 +45,10 @@ export default function Donation(props) {
 				</Toolbar>
 			</AppBar>
 
-			<Card className={classes.root1}>
-				<CardActionArea>
-					<CardMedia className={classes.media1} />
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Donation-1
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							100 taka
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary" className={classes.btn} onClick={() => { handleRoute(1) }}>
-						Donate
-					</Button>
-				</CardActions>
-			</Card>
-			<br />
 
 
 
-			<Card className={classes.root2}>
-				<CardActionArea>
-					<CardMedia className={classes.media2} />
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Donation-2
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							300 taka
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary" className={classes.btn} onClick={() => { handleRoute(2) }}>
-						Donate
-					</Button>
-				</CardActions>
-			</Card>
-			<br />
+
 
 			<Card className={classes.root3}>
 				<CardActionArea>
@@ -95,7 +58,7 @@ export default function Donation(props) {
 							Donation-3
 						</Typography>
 						<Typography variant="body2" color="textSecondary" component="p">
-							500 taka
+							100 taka
 						</Typography>
 					</CardContent>
 				</CardActionArea>
