92e532b6ef072ad417ca022463d8b527754778c8
Labnan <khalidmasum@iut-dhaka.edu>
2021-10-16 22:52:06 +0600

    Implement Logic for dynamicly adding weather data

--- frontend/react-main/src/pages/weather.js
+++ frontend/react-main/src/pages/weather.js
@@ -6,6 +6,8 @@ import {CityWeatherDataCard} from "./Weather/Components/cityWeatherDataCard";
 import Midnoon from "../images/noon4.jpg";
 import Storm from "../images/Storm.jpg";
 
+import {rgbToHex} from "@material-ui/core";
+
 
 
 
@@ -53,7 +55,7 @@ class Weather extends Component {
     onSubmit(event) {
         event.preventDefault()
         const cityValue = document.getElementById("city").value;
-        axios.post('http://localhost:8001/getWeather', {city: cityValue})
+        axios.post('http://localhost:8001/getWeather/hourly', {city: cityValue})
             .then((response) => {
                 console.log(response.data);
                 this.setState({
@@ -79,7 +81,7 @@ class Weather extends Component {
         if (this.submitted) return;
         this.submitted = true;
 
-        axios.get('http://localhost:8001/getWeather')
+        axios.get('http://localhost:8001/getWeather/hourly')
             .then((response) => {
                 console.log(response.data);
                 this.setState({
@@ -106,10 +108,10 @@ class Weather extends Component {
         return (
             <div>
                 <BaseNavBar/>
-                <div style={{marginTop: 80}}/>
+                <div style={{marginTop: 80} }/>
                 <Container className="d-flex vh-100">
                     <Row className="m-auto ">
-                        <Card style={{width: window.innerWidth - 300}}>
+                        <Card className="transparentBG" style={{width: window.innerWidth - 300}}>
 
 
                             <Card.Title>
@@ -151,7 +153,10 @@ class Weather extends Component {
                                     this.state.cityData.currently !== undefined ?
 
                                         (<CityWeatherDataCard cityName={this.state.cityName}
-                                                              cityData={this.state.cityData}/>
+                                                              cityData={this.state.cityData}
+                                                              index={0}
+
+                                            />
 
                                         ) :
 
@@ -170,7 +175,10 @@ class Weather extends Component {
                                     this.state.cityData.currently !== undefined ?
 
                                         (<CityWeatherDataCard cityName={this.state.cityName}
-                                                              cityData={this.state.cityData}/>
+                                                              cityData={this.state.cityData}
+                                                              index={1}
+
+                                            />
 
                                         ) :
 
